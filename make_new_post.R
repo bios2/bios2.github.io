1ea9a534e3226ee2e6c97df04f5556ce9d61295d
graciellehigino <graciellehigino@hotmail.com>
2021-04-28 10:23:51 -0400

    :tada: HUGO pt2

--- make_new_post.R
+++ make_new_post.R
@@ -1,5 +1,5 @@
-distill::create_post(title = "Making websites with HUGO, Part 1",
+distill::create_post(title = "Making websites with HUGO, Part 2",
                      author = c("Dominique Gravel, Guillaume Larocque"),
                      collection = "posts",
-                     date = lubridate::ymd("2020-12-07")
+                     date = lubridate::ymd("2020-12-08")
 )
