8e5a21aea4ed986a2e4b04c97da5ab8dc16d663c
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-08-11 10:45:52 -0300

    biodiv modelling course folders

--- make_new_post.R
+++ make_new_post.R
@@ -5,3 +5,14 @@ distill::create_post(title = "Introduction to Shiny Apps",
                      edit = TRUE,
                      draft = TRUE
 )
+
+
+## for a longer training:
+#
+distill::create_post(title = "Day 5",
+                     author = "Dominique Gravel, Andrew MacDonald",
+                     collection = "BiodiversityModelling",
+                     date = lubridate::ymd("2021-08-16"),
+                     edit = FALSE,
+                     draft = TRUE
+)
